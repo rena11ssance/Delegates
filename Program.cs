0c4bac92da2d93198d72b24a95995b6fa9abbada
lxst <lxst@YEGOR>
2025-10-31 20:48:07 +0500

    code refactoring

--- Program.cs
+++ Program.cs
@@ -11,12 +11,12 @@ namespace Delegates
             Console.Write("Введите радиус: ");
             double radius = Convert.ToDouble(Console.ReadLine());
 
-            MyDelegate delegateForAll = Circumference;
+            MyDelegate delegateForAll = circumference;
             delegateForAll += areaOfCircle;
             delegateForAll += volumeOfBall;
             
             List<double> resultsForDelegates = new List<double>();
-            string[] namesBeforePrint = new string[3] {"Длина окружности равна = ", "Площадь круга равна = ", "Объем шара равен = "};
+            string[] titlesBeforePrint = new string[3] {"Длина окружности равна = ", "Площадь круга равна = ", "Объем шара равен = "};
 
             foreach (MyDelegate results in delegateForAll.GetInvocationList())
             {
@@ -25,11 +25,11 @@ namespace Delegates
 
             for (int i = 0; i < resultsForDelegates.Count; i++)
             {
-                Console.WriteLine($"{namesBeforePrint[i]}{resultsForDelegates[i]:F2}.");
+                Console.WriteLine($"{titlesBeforePrint[i]}{resultsForDelegates[i]:F2}.");
             }
         }
 
-        static double Circumference(double r) => 2 * Math.PI * r;
+        static double circumference(double r) => 2 * Math.PI * r;
         static double areaOf​​Circle(double r) => Math.PI * Math.Pow(r, 2);
         static double volumeOfBall(double r) => 4.0 / 3.0 * Math.PI * Math.Pow(r, 3);
     }
